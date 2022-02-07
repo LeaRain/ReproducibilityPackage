e29345fd2f6e418db630652b32854c51a34d11aa
lea <blue.blood@gmx.de>
2022-02-07 15:42:56 +0100

    Add some documentation

--- scripts/generate_comparison_plots.py
+++ scripts/generate_comparison_plots.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Define global parameters for further usage
 data = pd.DataFrame(columns=["step", "value", "structure", "extraction", "encoding", "run"])
 run_count = 1
 lockwood = "Lockwood and Si."
@@ -26,6 +27,10 @@ def read_csv_data(file_path):
 
 
 def add_csv_data_to_data_frame(file_path):
+    """
+    Get a csv data file and add the data to the global data frame with checking for some meta information.
+    """
+
     global run_count
     global data
 
@@ -33,6 +38,7 @@ def add_csv_data_to_data_frame(file_path):
 
     plain_data["step"] = plain_data["step"].mul(100)
 
+    # Get the structure of the data.
     if "lockwood" in file_path:
         structure = lockwood
     else:
@@ -40,6 +46,7 @@ def add_csv_data_to_data_frame(file_path):
 
     plain_data["structure"] = structure
 
+    # Get the extraction of the data.
     if ".gs." in file_path:
         extraction = gs
     elif ".ls." in file_path:
@@ -49,6 +56,7 @@ def add_csv_data_to_data_frame(file_path):
 
     plain_data["extraction"] = extraction
 
+    # Get the encoding of the data.
     if ".c_enc" in file_path:
         encoding = c_enc
     elif ".sc_enc" in file_path:
@@ -58,10 +66,12 @@ def add_csv_data_to_data_frame(file_path):
 
     plain_data["encoding"] = encoding
 
+    # Get the current run count, incremented for every data file.
     plain_data["run"] = run_count
     run_count += 1
 
     # dataframe.append() is deprecated now, pandas.concat() is the recommended new method
+    # add modified data to frame
     data = pd.concat([data, plain_data])
 
 
@@ -72,11 +82,19 @@ def plot_data():
 
 
 def plot_data_for_structure_and_extraction(structure, extraction):
+    """
+    Plot the data for one given structure and extraction: The plot contains five different runs and a summary of those
+    five runs per encoding.
+    """
+
+    # Filter for the given structure and extraction.
     structure_extraction_data = data.loc[(data["structure"] == structure) & (data["extraction"] == extraction)]
+    # Calculate the mean of every run for all three encoding types.
     mean_data = pd.pivot_table(structure_extraction_data.reset_index(), index="step", columns=["encoding"],
                                values="value")
 
     for encoding in [c_enc, sc_enc, sd_enc]:
+        # Plot the five runs.
         encoding_data = extract_encoding_data(structure_extraction_data, encoding)
         plt.plot(encoding_data, color=colors[encoding], alpha=0.1)
         if not mean_data.empty:
@@ -86,6 +104,10 @@ def plot_data_for_structure_and_extraction(structure, extraction):
 
 
 def extract_encoding_data(extraction_data, encoding):
+    """
+    Extract the data for one specific encoding type and prepare it for further processing.
+    """
+
     encoding_data = extraction_data.loc[(extraction_data["encoding"] == encoding)]
     encoding_data = pd.pivot_table(encoding_data.reset_index(), index="step", columns=["run"], values="value")
     return encoding_data
