f7868fe1061bd8b77dfcf2f7d4da0f676b9319c2
Martin Meilinger <Martin-Meilinger@gmx.de>
2022-02-16 19:56:53 +0100

    inverts the semantics of the argument for easier understanding

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-echo $1
-
-if [ "$1" == "true" ]; then
-	echo "generate plots"
+if [ "$1" == "false" ]; then
+	echo "only generating plots..."
 	./generate_all.sh
 else
-	echo "run training"
+	echo "running trainings..."
 	./run_all_generate_all.sh
 fi
